ed52ad8c8796357c53f78d1bb01c4f13ff11bc41
win5948 <win5948@gmail.com>
2023-11-14 09:37:23 +0900

    setting

---
+++ src/main/java/com/example/sb20231114/Sb20231114Application.java
@@ -0,0 +1,13 @@
+package com.example.sb20231114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb20231114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb20231114Application.class, args);
+    }
+
+}
